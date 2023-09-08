637e05a61a867305044e32ffa55c817e119ebe73
Kevin Kim <98494454+kevink0908@users.noreply.github.com>
2023-09-08 00:53:06 -0700

    Add files via upload
    Finished Ch 1&2 (without running on my hardware device).

--- MainActivity.kt
+++ MainActivity.kt
@@ -5,34 +5,68 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import com.bignerdranch.android.geoquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity()
 {
-    private lateinit var trueButton: Button
-    private lateinit var falseButton: Button
+    // create binding and a list of Question objects.
+    private lateinit var binding: ActivityMainBinding
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true))
+    private var currentIndex = 0
 
+    // this function initializes the buttons and "listens for"
+    // a button to be clicked on.
+    // NOTE: we are saying "Bundle?" since the Bundle might be null.
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // obtain a reference to the layout and inflate it in the same line
+        // by initializing "binding".
+        // NOTE: ActivityMainBinding will allow you to get references
+        //       to each UI element in your activity_main.xml layout.
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
-
-        trueButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
-                R.string.correct_toast,
-                Toast.LENGTH_SHORT
-            ).show()
+        binding.trueButton.setOnClickListener { view: View ->
+            checkAnswer(true)
         }
 
-        falseButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
-                R.string.incorrect_toast,
-                Toast.LENGTH_SHORT
-            ).show()
+        binding.falseButton.setOnClickListener { view: View ->
+            checkAnswer(false)
         }
+
+        binding.prevButton.setOnClickListener {
+            currentIndex = (currentIndex - 1) % questionBank.size
+            updateQuestion()
+        }
+
+        binding.nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        updateQuestion()
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        binding.questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+            .show()
     }
 }
